d9809fc67fb06b3dda53a553ab0efb00df3f4921
tomand94 <tomand94@gmail.com>
2021-11-14 18:41:03 +0100

    cw3

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,76 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj liczbe: ");
+        int i = sc.nextInt();
+
+        switch (i % 2) {
+            case 0:
+                System.out.println("Parzysta");
+                break;
+            default:
+                System.out.println("Nieparzysta");
+                break;
+        }
+
+        System.out.println("Podaj liczbe: ");
+        int j = sc.nextInt();
+
+        switch (j % 7) {
+            case 0:
+                System.out.println("Podzielna");
+                break;
+            default:
+                System.out.println("Niepodzielna");
+                break;
+        }
+
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
 
+        String str = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        char[] actualCharArray = str.toCharArray();
 
+        int o = 0;
+        int p = 0;
+        int x = 0;
+        int z = 0;
+        int q = 0;
+        int def = 0;
+
+        for (char item: actualCharArray) {
+            switch (item) {
+                case 'o':
+                    o++;
+                    break;
+                case 'p':
+                    p++;
+                    break;
+                case 'x':
+                    x++;
+                    break;
+                case 'z':
+                    z++;
+                    break;
+                case 'q':
+                    q++;
+                    break;
+                default:
+                    def++;
+                    break;
+            }
+        }
+
+                System.out.println("Liczba liter 'o' w wyrazie: " + o);
+                System.out.println("Liczba liter 'p' w wyrazie: " + p);
+                System.out.println("Liczba liter 'x' w wyrazie: " + x);
+                System.out.println("Liczba liter 'z' w wyrazie: " + z);
+                System.out.println("Liczba liter 'q' w wyrazie: " + q);
+                System.out.println("Reszta: " + def);
+        }
     }
-}
