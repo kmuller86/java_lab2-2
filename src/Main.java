f856129022240f3a2a0353576b61bd1538ca7084
tomand94 <tomand94@gmail.com>
2021-11-14 19:08:38 +0100

    cw4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +13,33 @@ public class Main {
         c) w trzeciej pętli return
         */
 
+        for (int i = 0; i < 10; i++) {
+            if ((i % 2 == 0) && (i != 0)){
+                System.out.println("tylko gdy jest parzysta");
+                continue;
+            }
+            System.out.println("tekst");
+        }
 
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Wpisz 'koniec' aby zakonczyc dzialanie petli.");
+            String test = sc.nextLine();
+            if (test.equals("koniec")){
+                break;
+            }
+            System.out.println("Sproboj ponownie.");
+        }
+
+
+        System.out.println("Kończy na 16");
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+            if (i == 16){
+                return;
+            }
+        }
+        
     }
 }
